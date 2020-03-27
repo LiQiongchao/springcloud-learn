1fda49ca470a6823eff1a456a2fd57ffa239db52
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-03-27 13:28:32 +0800

    Initial commit

---
+++ alibaba-nacos/src/test/java/com/oio/nacos/AlibabaNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oio.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
